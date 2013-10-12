eff377e4acee76844f6b885abbeb63068c44088e
fougeretom@gmail.com <pixeletom@gmail.com>
2013-10-12 22:32:23 +0200

    [PCE] Add class for HSV image

---
+++ PhotoColorEditor/imagehsv.cpp
@@ -0,0 +1,5 @@
+#include "imagehsv.h"
+
+imageHSV::imageHSV()
+{
+}
---
+++ PhotoColorEditor/imagehsv.h
@@ -0,0 +1,16 @@
+#ifndef IMAGEHSV_H
+#define IMAGEHSV_H
+
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+
+class imageHSV
+{
+public:
+    imageHSV();
+
+    cv::Mat _s_hsv;
+};
+
+#endif // IMAGEHSV_H
